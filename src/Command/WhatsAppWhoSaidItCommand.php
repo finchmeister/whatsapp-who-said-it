492aa13f0429390b15adc82311152ffc21546403
jfinch <thefinchmeister@gmail.com>
2018-02-12 16:29:35 +0000

    Better parsing

--- src/Command/WhatsAppWhoSaidItCommand.php
+++ src/Command/WhatsAppWhoSaidItCommand.php
@@ -54,17 +54,36 @@ class WhatsAppWhoSaidItCommand extends Command
         }
 
         $this->chat = file_get_contents($this->getWhatsAppExportDirectory().'/'.$filename);
-        $matches = $this->getChatParticipants();
-        print_r(array_unique($matches));
+        $matches = $this->parseWhatsAppExport();
+        $io->text(sprintf("%s messages", count($matches[1])));
+        $io->text($this->getMessageString(count($matches[1])-1, $matches));
+        print_r($this->getChatParticipants($matches[2]));
     }
 
-
-
-
-    protected function getChatParticipants()
+    protected function getMessageString($key, $matches)
     {
-        preg_match_all('/\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2} - (.+?):/', $this->chat, $matches);
-        return $matches[1];
+        return sprintf(
+            "%s - %s: %s \n",
+            $matches[1][$key],
+            $matches[2][$key],
+            $matches[3][$key]
+        );
+    }
+
+    protected function parseWhatsAppExport()
+    {
+        $append = date('d/m/Y, H:i'); // A bit hacky but makes the positive look ahead work for the final message
+        preg_match_all(
+            '/(\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}) - (.+?): (\X+?(?=\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}))/',
+            $this->chat.$append,
+            $matches
+        );
+        return $matches;
+    }
+
+    protected function getChatParticipants($matches)
+    {
+        return array_unique($matches);
     }
 
 
