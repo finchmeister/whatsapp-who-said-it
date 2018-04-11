727c4d864a708516dc0c9dcd3acf7c1f0f6f0828
Jonathan Finch <thefinchmeister@gmail.com>
2018-04-11 21:51:11 +0100

    template wip

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -16,4 +16,23 @@ class GameController extends Controller
             'controller_name' => 'GameController',
         ]);
     }
+
+    /**
+     * @Route("/question")
+     */
+    public function question()
+    {
+
+
+
+
+        return $this->render('game/question.html.twig', [
+            'question' => 'Who put a tenner in?',
+            'no_of_questions' => 10,
+            'current_question_no' => 4,
+            'score' => 2,
+        ]);
+    }
+
+
 }
