e4924c9965aab0807d64f3302af64b19157da066
jfinch <thefinchmeister@gmail.com>
2018-04-11 13:21:10 +0100

    Add front end stuff

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Whats App Who Said It
+
+## Installation
+```
+composer install
+yarn install
+yarn run encore dev
+```
+
+```
+php bin/console server:run
+```
---
+++ src/Controller/GameController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GameController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('game/index.html.twig', [
+            'controller_name' => 'GameController',
+        ]);
+    }
+}
